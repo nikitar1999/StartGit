c76fff99093752122cffbc68c404bab3d256be3f
mvkoroteev <mvkoroteev@fa.ru>
2019-03-14 11:03:13 +0300

    Added more code

--- app/src/main/java/com/example/startgit/MainActivity.java
+++ app/src/main/java/com/example/startgit/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.startgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
     }
 }
